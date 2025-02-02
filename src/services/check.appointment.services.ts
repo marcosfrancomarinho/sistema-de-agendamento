6f328501c024548e5d80f5565785be687dc5281e
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2025-02-02 20:51:32 -0300

    finalizando parte da logica de checagem de agendamento, falta a parte do horario de funcionamento das 8 as 18 horas terminar depois

--- src/services/check.appointment.services.ts
+++ src/services/check.appointment.services.ts
@@ -2,33 +2,36 @@ import { IResponseSearchDataBase, ISearchSchedulingAdapter } from "../@types/rep
 import { ICheckAppointmentServices } from "../@types/services/check.appointment.services";
 
 export class CheckAppointmentServices implements ICheckAppointmentServices {
-	private serviceTime: number = 30;
-	private currentHours: Date;
-	private searchSchedulingAdapter: ISearchSchedulingAdapter;
-
-	constructor(searchSchedulingAdapter: ISearchSchedulingAdapter) {
-		this.currentHours = new Date();
-		this.searchSchedulingAdapter = searchSchedulingAdapter;
-	}
+	private SERVICE_TIME: number = 30;
+	constructor(private searchSchedulingAdapter: ISearchSchedulingAdapter) {}
 	private pastsCheduleChecker = (scheduledDateAndTimes: Date): void => {
-		this.currentHours.setSeconds(0, 0);
-		if (scheduledDateAndTimes.getTime() <= this.currentHours.getTime())
-			throw new Error("não pode informar uma data passada ou já agendada.");
+		const currentHours: Date = new Date();
+		currentHours.setSeconds(0, 0);
+		if (scheduledDateAndTimes.getTime() <= currentHours.getTime()) throw new Error("não pode informar uma data passada.");
 	};
 	private reserveTime = (scheduledDateAndTimes: Date, indice: number) => {
-		const time: Date = new Date(
-			scheduledDateAndTimes.getFullYear(),
-			scheduledDateAndTimes.getMonth(),
-			scheduledDateAndTimes.getDate(),
-			scheduledDateAndTimes.getHours(),
-			scheduledDateAndTimes.getMinutes(),
-			0
-		);
-		time.setMinutes(time.getMinutes() + this.serviceTime * indice);
+		const time: Date = new Date(scheduledDateAndTimes.getTime());
+		time.setMinutes(time.getMinutes() + this.SERVICE_TIME * indice);
 		return time;
 	};
 	private businessHoursPicker = (requestedDateAndTime: Date): void => {
-		//logica referente aos horario comercial de funcionamento
+		const day: number = requestedDateAndTime.getDay();
+		const hours = requestedDateAndTime.getMinutes();
+		enum weekBusines {
+			saturday = 6,
+			sunday = 0,
+		}
+		enum hoursBusinnes {
+			start = 8,
+			end = 17,
+		}
+		if (day === weekBusines.saturday || day === weekBusines.sunday) {
+			throw new Error("sábado e domingo está fechado.");
+		}
+		//ainda falta fazer essa parte
+		if (hours < hoursBusinnes.start || hours > hoursBusinnes.end) {
+			throw new Error("agendamento apenas das 8h as 17h");
+		}
 	};
 	private appointmentAvailabilityChecker = (requestedDateAndTime: Date, scheduledDateAndTimes: Date): void => {
 		scheduledDateAndTimes.setSeconds(0, 0);
@@ -45,6 +48,7 @@ export class CheckAppointmentServices implements ICheckAppointmentServices {
 		try {
 			scheduledDateAndTimes.setSeconds(0, 0);
 			this.pastsCheduleChecker(scheduledDateAndTimes);
+			this.businessHoursPicker(scheduledDateAndTimes);
 			const responseQuerySearchDatabase: IResponseSearchDataBase[] = await this.searchSchedulingAdapter.selectDb();
 			if (responseQuerySearchDatabase.length === 0) return true;
 			for (const { datahours } of responseQuerySearchDatabase) {
