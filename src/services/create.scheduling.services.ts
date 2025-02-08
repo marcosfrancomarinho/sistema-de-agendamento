d27237c28aea1ea059388a0ffd39e2411f3bccfe
marcosfrancomarinho <marcosmarinho19998@gmail.com>
2025-02-08 19:39:24 -0300

    alteraçao na logica do agendamento

--- src/services/create.scheduling.services.ts
+++ src/services/create.scheduling.services.ts
@@ -3,7 +3,6 @@ import { ICheckAppointmentServices } from '../@types/services/check.appointment.
 import { ICreateSchedulingServices } from '../@types/services/create.scheduling.services';
 
 export class CreateSchedulingServices implements ICreateSchedulingServices {
-	private responseQueryInsertDatabase!: IQueryResult;
 	constructor(
 		private createSchedulingAdapter: ICreateSchedulingAdapter,
 		private checkAppointmentServices: ICheckAppointmentServices
@@ -11,8 +10,8 @@ export class CreateSchedulingServices implements ICreateSchedulingServices {
 	public add = async (name: string, email: string, phone: string, datahours: Date): Promise<IQueryResult> => {
 		try {
 			await this.checkAppointmentServices.check(datahours);
-			this.responseQueryInsertDatabase = await this.createSchedulingAdapter.insertDb(name, email, phone, datahours);
-			return this.responseQueryInsertDatabase;
+			const responseQueryInsertDatabase = await this.createSchedulingAdapter.insertDb(name, email, phone, datahours);
+			return responseQueryInsertDatabase;
 		} catch (error) {
 			throw error as Error;
 		}
